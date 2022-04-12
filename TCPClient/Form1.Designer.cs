334f05c2e91e56ecc78aeb1a7148ae51256464b9
AMIEian <nitin4july@gmail.com>
2022-04-12 16:48:04 +0530

    Add project files.

---
+++ TCPClient/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace TCPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(74, 26);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(287, 27);
+            this.txtHost.TabIndex = 0;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(425, 26);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(123, 27);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "500";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Host :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port :";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(573, 25);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(194, 29);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(21, 75);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(746, 113);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.Text = resources.GetString("txtMessage.Text");
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(21, 256);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(746, 182);
+            this.txtStatus.TabIndex = 7;
+            this.txtStatus.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.MinimumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TCPClient";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox txtStatus;
+    }
+}
+
---
+++ TCPClient/Form1.cs
@@ -0,0 +1,121 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPClient
+{
+    public partial class Form1 : Form
+    {
+        Thread dataSender;
+        bool startSend = false;
+        bool start = true;
+        public Form1()
+        {
+            InitializeComponent();
+            dataSender = new Thread(SendData);
+            dataSender.Start();
+        }
+
+        SimpleTcpClient client;
+        string[] epcs = { "30361FAC682922C000000005", "30361FAC682922C000000009", "30361FAC682922C00000000D","30361FAC682922C000000016","30361FAC682922C00000004E","30361FAC6828B5C000000007", "30361FAC6828B5C000000008", "30361FAC6828B5C00000000B", "30361FAC6828B5C00000000D", "30361FAC6828B5C00000000E", "30361FAC6828B5C000000014", "30361FAC6828B5C000000015", "30361FAC6828B5C000000024", "30361FAC6828B5C000000030", "30361FAC6828B5C00000004C", "30361FAC6828B5C00000005E", "30361FAC6828B6000000001F", "30361FAC6828B60000000025", "30361FAC6828B60000000027", "30361FAC6828B60000000033", "30361FAC6828B6000000003B", "30361FAC6828B6000000003C", "30361FAC6828B6000000003D" }; //, "30361FAC6828B6400000000D", "30361FAC6828B6400000001A", "30361FAC6828B6400000001E", "30361FAC6828B64000000023" };//, "30361FAC6824CA400000004B", "30361FAC6824C30000000057", "30361FAC6824C2800000001C", "30361FAC681ADF4000000045", "30361FAC681AE9800000004C", "30361FAC681C7D800000007A", "30361FAC681C7C4000000078", "30361FAC6824C9C00000001D", "30361FAC681C7BC00000007C", "30361FAC681ADF4000000046", "30361FAC681ADE8000000057", "30361FAC681AE84000000064", "30361FAC681B18000000005B", "30361FAC681E5CC000000021", "30361FAC681C7C0000000063", "30361FAC6824CA4000000027", "30361FAC681AE5C000000014", "30361FAC681AE6800000006C", "30361FAC681C7C000000001C" };
+        string ip;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+
+            string localIp = "0.0.0.0";
+            //Star server on local ip on port 500
+            try
+            {
+                localIp = GetLocalIPAddress();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            txtHost.Text = localIp;
+            ip = localIp;
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            //Update message to txtStatus
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Text += e.MessageString;
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            //Connect to server
+            client.Connect(txtHost.Text, Convert.ToInt32(txtPort.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (startSend == false)
+            {
+                startSend = true;
+            }
+            else
+            {
+                startSend = false;
+            }
+            System.Diagnostics.Debug.WriteLine(startSend.ToString());
+        }
+
+        void SendData()
+        {
+            while(start)
+            {
+                while (startSend)
+                {
+                    Random rnd = new Random();
+                    string data = epcs[rnd.Next(0, epcs.Length)] + ",A0001," + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt");
+                    txtMessage.Invoke((MethodInvoker)delegate ()
+                    {
+                        txtMessage.Text = data;
+                    });
+                    //client.Connect(ip, 500);
+                    client.WriteLineAndGetReply(data, TimeSpan.FromSeconds(0));
+                    //client.Disconnect();
+                    Thread.Sleep(10);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            start = false;
+            Application.Exit();
+        }
+    }
+}
